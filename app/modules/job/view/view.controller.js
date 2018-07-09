ac3aeb25674cfd84e60029ce0acc63965ca80f69
Israt farzana <m2farzana@gmail.com>
2018-07-09 16:42:57 +0100

    Full commit

--- app/modules/job/view/view.controller.js
+++ app/modules/job/view/view.controller.js
@@ -3,10 +3,24 @@
 
     angular
         .module('elogbooks.job')
-        .controller('JobViewController', ['jobResponse', JobViewController]);
+        .controller('JobViewController', ['$http', 'jobResponse', '$scope', JobViewController]);
 
-    function JobViewController(jobResponse) {
+    function JobViewController($http, jobResponse, $scope) {
         var vm = this;
-        vm.job = jobResponse;
+
+        vm.job = {
+            description : jobResponse.description,
+            status : jobResponse.status,
+            user_id : jobResponse.user_id
+        };
+
+       $scope.user = $http.get("http://localhost:8001/user/" + jobResponse.user_id)
+            .then(function(response) {
+                $scope.user = response.data;
+                console.log(response.data);
+
+            });
+
+        console.log(vm.job);
     }
 })();
